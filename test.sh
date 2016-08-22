873dfb1b3411f8c36b5a35016d611486206fddd9
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-22 15:26:03 -0700

    Check for required env variables in `test.sh`

--- test.sh
+++ test.sh
@@ -6,6 +6,16 @@
 
 set -ex
 
+if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
+	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
+	exit 1
+fi
+
+if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
+	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
+	exit 1
+fi
+
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io"} }}'
 
 ./vendor/bin/behat $*
