8012a8a5be5b6b3ba9885853ff2528f8d770323e
Steve Persch <stevector@users.noreply.github.com>
2016-08-22 15:06:22 -0500

    Move auth whoami check to bottom of check-required.sh

--- check-required.sh
+++ check-required.sh
@@ -4,12 +4,6 @@
 set +x
 set -e
 
-terminus auth whoami > /dev/null
-if [ $? -ne 0 ]; then
-	echo "Terminus unauthenticated; assuming unauthenticated build"
-	exit 0
-fi
-
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
@@ -18,4 +12,12 @@ fi
 if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
 	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
 	exit 1
+fi
+
+# Builds from forks will not have the Terminus token available.
+# Scripts should exit, but not with an error.
+terminus auth whoami > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
 fi
