92a69ed8e8dcb680492893e490a9ee16ecfad7ed
Steve Persch <steve.persch@pantheon.io>
2016-08-22 16:05:14 -0500

    Revert "Moving back"
    This reverts commit 2f752d64edf59d23e0ff3db207b2d4390ee9a5ae.

--- check-required.sh
+++ check-required.sh
@@ -4,14 +4,6 @@
 set +x
 set -e
 
-# Builds from forks will not have the Terminus token available.
-# Scripts should exit, but not with an error.
-terminus auth whoami > /dev/null
-if [ $? -ne 0 ]; then
-	echo "Terminus unauthenticated; assuming unauthenticated build"
-	exit 0
-fi
-
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
@@ -20,4 +12,12 @@ fi
 if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
 	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
 	exit 1
+fi
+
+# Builds from forks will not have the Terminus token available.
+# Scripts should exit, but not with an error.
+terminus auth whoami > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
 fi
