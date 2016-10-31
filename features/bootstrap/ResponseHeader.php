8b92ce0f6144ed607f50dd2903282369618c9b7f
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-31 15:18:33 -0700

    Add steps and basic tests for response headers

---
+++ features/bootstrap/ResponseHeader.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PantheonSystems\PantheonWordPressUpstreamTests\Behat;
+
+use Behat\MinkExtension\Context\RawMinkContext;
+
+class ResponseHeader extends RawMinkContext {
+
+    /**
+     * Checks, that current page response header is equal to specified.
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should be "(?P<value>(?:[^"]|\\")*)"$/
+     */
+    public function assertResponseHeader($name, $value)
+    {
+        $this->assertSession()->responseHeaderEquals($name, $value);
+    }
+
+    /**
+     * Checks, that current page response header is not equal to specified.
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should not be "(?P<value>(?:[^"]|\\")*)"$/
+     */
+    public function assertResponseHeaderIsNot($name, $value)
+    {
+        $this->assertSession()->responseHeaderNotEquals($name, $value);
+    }
+
+    /**
+     * Checks that the current page response header is set
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should exist.
+     */
+    public function assertResponseHeaderExists($name) {
+        $this->assertSession()->responseHeaderMatches($name,'/^.{1,}$/');
+    }
+
+    /**
+     * Checks that the current page response header is not set
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should not exist.
+     */
+    public function assertResponseHeaderNotExists($name) {
+        $this->assertSession()->responseHeaderMatches($name,'/^.{0}$/');
+    }
+
+    /**
+     * Checks, that current page response header contains specified value.
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should contain "(?P<value>(?:[^"]|\\")*)"$/
+     */
+    public function assertResponseHeaderContains($name, $value)
+    {
+        $this->assertSession()->responseHeaderContains($name, $value);
+    }
+    /**
+     * Checks, that current page response header does not contain specified value.
+     *
+     * @Then /^the response header "(?P<name>(?:[^"]|\\")*)" should not contain "(?P<value>(?:[^"]|\\")*)"$/
+     */
+    public function assertResponseHeaderNotContains($name, $value)
+    {
+        $this->assertSession()->responseHeaderNotContains($name, $value);
+    }
+
+}
---
+++ features/pantheon-logged-out.feature
@@ -0,0 +1,13 @@
+Feature: Verify various Pantheon features as a logged-out user
+
+  Scenario: Cache-Control should default to TTL=600
+    When I go to "/"
+    Then the response header "Cache-Control" should exist
+    And the response header "Cache-Control" should contain "max-age=600"
+
+  Scenario: Cache-Control should have "nocache" for logged-in users
+    Given I log in as an admin
+
+    When I go to "/"
+    And the response header "Cache-Control" should contain "nocache"
+    And the response header "Cache-Control" should not contain "max-age=600"
