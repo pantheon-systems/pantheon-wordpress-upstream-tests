545f417e6fa904dc26f61546994ebdefe497caad
Phil Tyler <phil.tyler@pantheon.io>
2023-08-10 16:18:03 -0700

    Revert "prepare from chris's PR"
    This reverts commit c179481be561d37cd339ef5c3b5951ff9dc4d50e.

--- prepare.sh
+++ prepare.sh
@@ -13,31 +13,11 @@ if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	exit 1
 fi
 
-# Duisable PasswordAuthentication on *.drush.in
-echo "Host *.drush.in
-	HostKeyAlgorithms +ssh-rsa" >> ~/.ssh/config
-
-terminus auth:login --machine-token=$TERMINUS_TOKEN
-
-# Apply any upstream updates (update WP core)
-terminus upstream:updates:apply $TERMINUS_SITE.dev --accept-upstream
-
-###
-# Switch to SFTP mode so the site can install plugins and themes
-###
-terminus connection:set $TERMINUS_SITE.dev sftp
-
-# Update plugins and themes to make sure we're 100% up-to-date.
-terminus wp -- $TERMINUS_SITE.dev plugin update --all
-terminus wp -- $TERMINUS_SITE.dev theme update --all
-# Commit the changes to the fixture.
-terminus env:commit $TERMINUS_SITE.dev --message="Update WordPress plugins and themes"
-
 ###
 # Create a new environment for this particular test run.
 ###
 terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
-terminus wp -- $TERMINUS_SITE.$TERMINUS_ENV theme activate twentyseventeen
+terminus env:wipe $SITE_ENV --yes
 
 ###
 # Get all necessary environment details.
@@ -47,5 +27,19 @@ PANTHEON_SITE_URL="$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io"
 PREPARE_DIR="/tmp/$TERMINUS_ENV-$TERMINUS_SITE"
 BASH_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+###
+# Switch to git mode for pushing the files up
+###
+terminus connection:set $SITE_ENV git
 rm -rf $PREPARE_DIR
 # git clone -b $TERMINUS_ENV $PANTHEON_GIT_URL $PREPARE_DIR
+
+###
+# Push the upstream branch to the environment
+###
+
+terminus remote:wp -- theme activate twentyseventeen
+###
+# Switch to SFTP mode so the site can install plugins and themes
+###
+terminus connection:set $SITE_ENV sftp
